0283073b871d8d2dfcdee0a274b45719e15e40c7
varun-wahi <wahivarun02@gmail.com>
2026-04-07 17:15:20 +0530

    faltu changes

--- android/app/.cxx/Debug/27536d51/armeabi-v7a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
+++ android/app/.cxx/Debug/27536d51/armeabi-v7a/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Darwin-24.4.0")
+set(CMAKE_HOST_SYSTEM "Darwin-25.3.0")
 set(CMAKE_HOST_SYSTEM_NAME "Darwin")
-set(CMAKE_HOST_SYSTEM_VERSION "24.4.0")
+set(CMAKE_HOST_SYSTEM_VERSION "25.3.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
 
 include("/Users/varunwahi/Library/Android/sdk/ndk/26.3.11579264/build/cmake/android.toolchain.cmake")
